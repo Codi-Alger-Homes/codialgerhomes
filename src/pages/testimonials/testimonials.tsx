d3fb362b34d9a92f45b8b885745527ce153dc66d
Sabrina Stangler <sabrinastangler51@gmail.com>
2024-08-30 13:49:10 -0500

    add links to google reviews on testimonials page

--- src/pages/testimonials/testimonials.tsx
+++ src/pages/testimonials/testimonials.tsx
@@ -16,26 +16,42 @@ export function Testimonials() {
     <PageContainer>
       <h1>Testimonials</h1>
       <div className='testimonials-container'>
-        <ClientReview review={HOME_REVIEWS[0]} />
-        <ClientReview review={HOME_REVIEWS[1]} />
-        <ClientReview review={HOME_REVIEWS[2]} />
+        <a
+          href='https://www.google.com/maps/place/Codi+Alger+Homes/@43.1181905,-88.2283668,17z/data=!3m1!4b1!4m6!3m5!1s0x880509c333b89bb3:0xa9698256dfcc75b5!8m2!3d43.1181905!4d-88.2283668!16s%2Fg%2F11j37lp8rl?entry=ttu'
+          className='testimonial-link'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Leave your own review on Google {'>'}
+        </a>
+        <ClientReview key='client-review-1' review={HOME_REVIEWS[0]} />
+        <ClientReview key='client-review-2' review={HOME_REVIEWS[1]} />
+        <ClientReview key='client-review-3' review={HOME_REVIEWS[2]} />
         <div className='testimonials-photo-row'>
           <img src={Sold1} alt='sold house' className='testimonials-photo' />
           <img src={Sold3} alt='sold house' className='testimonials-photo' />
           <img src={Sold2} alt='sold house' className='testimonials-photo' />
         </div>
-        <ClientReview review={BUYERS_REVIEWS[0]} />
-        <ClientReview review={BUYERS_REVIEWS[1]} />
-        <ClientReview review={BUYERS_REVIEWS[2]} />
-        <ClientReview review={BUYERS_REVIEWS[3]} />
+        <ClientReview key='client-review-4' review={BUYERS_REVIEWS[0]} />
+        <ClientReview key='client-review-5' review={BUYERS_REVIEWS[1]} />
+        <ClientReview key='client-review-6' review={BUYERS_REVIEWS[2]} />
+        <ClientReview key='client-review-7' review={BUYERS_REVIEWS[3]} />
         <div className='testimonials-photo-row'>
           <img src={Sold7} alt='sold house' className='testimonials-photo' />
           <img src={Sold8} alt='sold house' className='testimonials-photo' />
           <img src={Sold9} alt='sold house' className='testimonials-photo' />
         </div>
-        <ClientReview review={SELLER_REVIEWS[0]} />
-        <ClientReview review={SELLER_REVIEWS[1]} />
-        <ClientReview review={SELLER_REVIEWS[2]} />
+        <ClientReview key='client-review-9' review={SELLER_REVIEWS[0]} />
+        <ClientReview key='client-review-10' review={SELLER_REVIEWS[1]} />
+        <ClientReview key='client-review-11' review={SELLER_REVIEWS[2]} />
+        <a
+          href='https://www.google.com/maps/place/Codi+Alger+Homes/@43.1181905,-88.2283668,17z/data=!3m1!4b1!4m6!3m5!1s0x880509c333b89bb3:0xa9698256dfcc75b5!8m2!3d43.1181905!4d-88.2283668!16s%2Fg%2F11j37lp8rl?entry=ttu'
+          className='link'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Read more reviews on Google {'>'}
+        </a>
       </div>
     </PageContainer>
   )
